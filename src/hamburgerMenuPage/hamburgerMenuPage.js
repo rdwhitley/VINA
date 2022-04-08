75104360b5552f753827e3bf63b7d38422e2a79c
Rafiq <whitleyrafiq95@gmail.com>
2022-04-08 15:28:54 -0400

    Logo placehodler added

--- src/hamburgerMenuPage/hamburgerMenuPage.js
+++ src/hamburgerMenuPage/hamburgerMenuPage.js
@@ -24,10 +24,11 @@ class hamburgerMenuPage extends Component {
 
   render() {
     return (
-        <div>
-        <nav className="navbar navbar-light bg-light">
+        <div data-mdb-right="true" style={{width: "100vw"}}>
+        <nav className="navbar navbar-light bg-light" data-mdb-right="true">
   <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-mdb-toggle="collapse"
+    <h1>LOGO</h1>
+    <button className="navbar-toggler ms-auto" type="button" data-mdb-toggle="collapse"
       data-mdb-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
       aria-expanded="false" aria-label="Toggle navigation">
       <i className="fas fa-bars"></i>
@@ -36,9 +37,9 @@ class hamburgerMenuPage extends Component {
 </nav>
 <div className="collapse" id="navbarToggleExternalContent">
   <div className="bg-light shadow-3 p-4">
-    <button className="btn btn-link btn-block border-bottom m-0">Link 1</button>
-    <button className="btn btn-link btn-block border-bottom m-0">Link 2</button>
-    <button className="btn btn-link btn-block m-0">Link 3</button>
+    <button className="btn btn-link btn-block border-bottom m-0">About</button>
+    <button className="btn btn-link btn-block border-bottom m-0">Contact</button>
+    <button className="btn btn-link btn-block m-0">Login</button>
   </div>
 </div>
 </div>
