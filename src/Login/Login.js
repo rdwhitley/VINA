28bacc2508893f1f940ea73f74ff828be08a5c2c
Rafiq <whitleyrafiq95@gmail.com>
2022-04-08 20:41:34 -0400

    login component is almost finished needs some assets for the background picture

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -7,19 +7,30 @@ import {
   MDBBtn,
   MDBIcon
 } from 'mdb-react-ui-kit';
+import {Container,Row,Col, Navbar} from  "react-bootstrap";
 const Login = () => {
-    return <form style={{border: "2px solid red", width: "50%", marginTop: "2em"}}>
-    <MDBInput className='mb-4' type='Username' id='Username' label='Username' />
-    <MDBInput className='mb-4' type='password' id='form2Example2' label='Password' />
+    return <div style={{width: "70vw", marginTop: "2em", border: "2px solid red", backgroundColor: "blue"}}>
+    <h3 style={{color: "white"}}>Login</h3>
+    <form className="card" style={{backgroundColor: "#adb9c7", boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.5)"}}>
+    
+    <Col style={{width: "50vw", margin: "0 auto", color: "#465c76"}}>
+    <div style={{margin: "0 auto"}}>
+    <label for="Username" >Username: </label>
+    <MDBInput className='mb-4' type='Username' id='Username' style={{backgroundColor: "white"}}/>
+    </div>
 
-    <MDBRow className='mb-4'>
-      
-<button type="button" class="btn btn-primary">Need Help</button>
-
-<button type="button" class="btn btn-primary">Login</button>
-        Login
-    </MDBRow>
+    <div style={{margin: "0 auto"}}>
+    <label for="Username">Password: </label>
+    <MDBInput className='mb-4' type='password' id='form2Example2' style={{backgroundColor: "white"}}/>
+    </div>
+    
+    <div style={{display: "flex"}}>
+    <div type="button" className="">Need Help?</div>
+    <button type="button" className="btn btn-rounded" style={{marginLeft: "auto",backgroundColor: "#6a727a", color: "white", marginBottom: "1em"}}>Login</button>
+    </div>
+    </Col>
   </form>
+  </div>
 };
 
 export default Login;
